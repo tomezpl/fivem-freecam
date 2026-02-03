0b79490157e4c995f8b8ce30890e9e070035e925
Tomasz Zajac <tomezpl@gmail.com>
2026-02-03 23:57:45 +0000

    feat(client): added sideways motion, roll, FOV, info output

--- client.js
+++ client.js
@@ -69,14 +69,31 @@ function createCamera() {
     return camHandle;
 }
 
+function vecToStr([x, y, z]) {
+    return `X = ${x}, Y = ${y}, Z = ${z}`;
+}
+
 on('onClientResourceStart', (resource) => {
     if (resource === ResourceName) {
         Logger.log('resource started');
         State.camera = createCamera();
 
-        RegisterCommand("cam", (params) => {
-            Logger.log('params', params);
-            State.setCameraEnabled(!State.isEnabled);
+        RegisterCommand("cam", (source, [cmd]) => {
+            let shouldEnable = !State.isEnabled;
+
+            switch(cmd) {
+                case 'on':
+                    shouldEnable = true;
+                    break;
+                case 'off':
+                    shouldEnable = false;
+                    break;
+                case 'info':
+                    Logger.log(`current cam\n\tpos: ${vecToStr(GetCamCoord(State.camera))}\n\trot: ${vecToStr(GetCamRot(State.camera, 2))}\n\tfov: ${GetCamFov(State.camera)}`);
+                    return;
+            }
+
+            State.setCameraEnabled(shouldEnable);
         })
     }
 });
@@ -84,10 +101,10 @@ on('onClientResourceStart', (resource) => {
 const Controls = Object.freeze({
     LookHoriz: 1,
     LookVert: 2,
-    FovUp: 252, // 314,
-    FovDown: 253, // 315,
-    MoveUp: 206, // 316,
-    MoveDown: 205, // 317,
+    FovUp: 252,
+    FovDown: 253,
+    MoveUp: 206,
+    MoveDown: 205,
     RollLeft: 189,
     RollRight: 190,
     MoveFwd: 196,
@@ -96,19 +113,11 @@ const Controls = Object.freeze({
 
 const ControlsToDisable = Object.values(Controls);
 
-const CameraLinearSpeed = 4;
+const CameraLinearSpeed = 0.8;
 const CameraAngularSpeed = 135;
+const CameraFovSpeed = 40;
 
 const DEG_TO_RAD = Math.PI / 180;
-/*
-function rotateVector(vecX, vecY, vecZ, yaw, pitch) {
-    const yawRad = yaw * DEG_TO_RAD;
-    const pitchRad = pitch * DEG_TO_RAD;
-
-    const rotMat = [
-        []
-    ]
-}*/
 
 function crossProduct(aX, aY, aZ, bX, bY, bZ) {
     return [(aY * bZ) - (aZ * bY), (aZ * bX) - (aX * bZ), (aX * bY) - (aY * bX)];
@@ -123,41 +132,48 @@ setTick(() => {
         const deltaTime = GetFrameTime();
 
         const forward = -GetDisabledControlNormal(0, Controls.MoveFwd);
-        const right = GetDisabledControlNormal(0, Controls.MoveRight);
+        const right = -GetDisabledControlNormal(0, Controls.MoveRight);
         const lookRight = -GetDisabledControlNormal(0, Controls.LookHoriz);
         const lookUp = -GetDisabledControlNormal(0, Controls.LookVert);
-
-        if(lookRight || lookUp) {
-            Logger.log(`input: (${lookRight}, ${lookUp})`);
-        }
+        const rollRight = GetDisabledControlNormal(0, Controls.RollRight);
+        const rollLeft = GetDisabledControlNormal(0, Controls.RollLeft);
+        const up = -GetDisabledControlNormal(0, Controls.MoveUp) + GetDisabledControlNormal(0, Controls.MoveDown);
+        const fovDelta = -GetDisabledControlNormal(0, Controls.FovUp) + GetDisabledControlNormal(0, Controls.FovDown);
 
         const [originX, originY, originZ] = GetCamCoord(State.camera);
         let [pitch, roll, yaw] = GetCamRot(State.camera, 2);
+        const fov = GetCamFov(State.camera);
+
         yaw += CameraAngularSpeed * lookRight * deltaTime;
         pitch += CameraAngularSpeed * lookUp * deltaTime;
-        const globalFwd = [1, 0, 0];
-        const globalRgt = [0, 1, 0];
+        roll += CameraAngularSpeed * (rollRight - rollLeft) * 0.5 * deltaTime;
 
         const yawRad = yaw * DEG_TO_RAD;
         const pitchRad = pitch * DEG_TO_RAD;
+        const rollRad = roll * DEG_TO_RAD;
 
-        // TODO: sideways motion
         const forwardX = Math.cos(yawRad) * Math.cos(pitchRad)
         const forwardY = Math.sin(yawRad) * -Math.cos(pitchRad);
         const forwardZ = Math.sin(pitchRad);
+
+        const rightX = Math.cos(yawRad + Math.PI * 0.5) * Math.cos(pitchRad);
+        const rightY = Math.sin(yawRad + Math.PI * 0.5) * -Math.cos(pitchRad);
+        const rightZ = Math.sin(pitchRad) * Math.sin(rollRad);
+
+        const [upX, upY, upZ] = crossProduct(forwardX, forwardY, forwardZ, rightX, rightY, rightZ);
+
         SetCamRot(State.camera, pitch, roll, yaw, 2);
-        // PointCamAtCoord(State.camera, originX + offsetX * 10, originY + offsetY * 10, originZ + offsetZ * 10);
 
         SetCamCoord(
             State.camera,
-            originX + (forwardY * CameraLinearSpeed * forward),
-            originY + (forwardX * CameraLinearSpeed * forward),
-            originZ + (forwardZ * CameraLinearSpeed * forward),
+            originX + (forwardY * CameraLinearSpeed * forward) + (rightY * CameraLinearSpeed * right) + (upY * CameraLinearSpeed * up),
+            originY + (forwardX * CameraLinearSpeed * forward) + (rightX * CameraLinearSpeed * right) + (upX * CameraLinearSpeed * up),
+            originZ + (forwardZ * CameraLinearSpeed * forward) + (rightZ * CameraLinearSpeed * right) + (upZ * CameraLinearSpeed * up),
         );
 
+        SetCamFov(State.camera, fov - fovDelta * deltaTime * CameraFovSpeed);
+
         RenderScriptCams(true, false, 0, false, false);
-        // const localFwd = globalFwd.map((c, i) => c * [Math.sin(yawRad), Math.cos(yawRad), 0][i])
-        // const localRgt = globalRgt.map((c, i) => c * [Math.sin(pitchRad), Math.cos(pitchRad), 0])[i]
     }
 })
 
